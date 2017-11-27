2cfd81fa5236fdb05611e08d586b6151bbe7b536
Dheeraj <dheeraj@habilelabs.io>
2017-11-27 18:19:34 +0530

    Demo app

---
+++ my-app/src/test.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Logos extends Component {
+    render(){
+        return (
+            <div>
+                Hello World Whatsupvffdv dfd
+            </div>
+        )
+    }
+}
+
+export default Logos;
